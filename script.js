b571be26fca15627b414cf396de937f04f683dda
Dmitry <you@example.com>
2021-12-06 22:35:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const headerList = document.querySelector(".nav-list");
+const headerBurger = document.querySelector(".header-burger");
+const body = document.querySelector("body");
+
+headerBurger.onclick = function(){
+  headerList.classList.toggle("active");
+  headerBurger.classList.toggle("active");
+  body.classList.toggle("lock");
+}
