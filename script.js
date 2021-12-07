452d4636d351f777545be7e5673d70e17cc309a7
Dmitry <you@example.com>
2021-12-07 10:31:23 +0300

    slider-slick

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ const headerBurger = document.querySelector(".header-burger");
 const body = document.querySelector("body");
 
 headerBurger.onclick = function(){
-  headerList.classList.toggle("active");
-  headerBurger.classList.toggle("active");
-  body.classList.toggle("lock");
+ 	headerList.classList.toggle("active");
+  	headerBurger.classList.toggle("active");
+ 	 body.classList.toggle("lock");
 }
+
+
+/* jQuery slider */
+$(document).ready(function(){
+	$('.slider').slick({
+		slidesToShow:3,
+		centerMode:true,
+		variableWidth:true,
+	});
+});
